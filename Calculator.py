3b81b752c9cf983e31ec3a066af4d1646a97c80d
Braindeadxd <64861077+Braindeadxd@users.noreply.github.com>
2021-10-04 23:38:50 +0300

    123
    123

--- Calculator.py
+++ Calculator.py
@@ -5,11 +5,22 @@ from sympy.parsing.sympy_parser import parse_expr
 class Calculator:
 
     # метод решения уравнений
-    def solveEq(self, eq_left, eq_right):
+    @staticmethod
+    def solveEq(eq_left, eq_right):
         a, b, y, x = symbols('a b y x')
         left = parse_expr(eq_left)
         right = parse_expr(eq_right)
         return solve(Eq(left, right), x)
+
+    @staticmethod
+    def solveEqi(eq):
+        a, b, y, x = symbols('a b y x')
+        expr = parse_expr(eq)
+        return solve(expr, x)
+
+    @staticmethod
+    def solveExp(exp):
+        return parse_expr(exp)
 
     @staticmethod
     def separateEq(eq):
--- interface.py
+++ interface.py
@@ -2,14 +2,16 @@ from Calculator import Calculator
 
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
 
 buttons = [
-    '7', '8', '9', '+', '*',
-    '4', '5', '6', '-', '/',
-    '1', '2', '3', '=', 'xⁿ',
-    '0', '.', '±','C',
-    'Exit', 'π', 'sin', 'cos',
-    '(', ')', 'n!', '√2', 'x', 'y']
+    '7', '8', '9', 'C',
+    '4', '5', '6', 'Exit',
+    '1', '2', '3', 'π',
+    '*', '0', '/', 'sin',
+    'cos', '(', ')', 'x',
+    '+', '-', '=', '.'
+]
 
 
 class Interface:
@@ -20,12 +22,29 @@ class Interface:
         self.root.title("Calculator")
         self.setButtons(self.root)
         self.calc_entry = Entry(self.root, width=33)
-        self.calc_entry.grid(row=0, column=0, columnspan=5)
+        self.calc_entry.grid(row=0, column=0, columnspan=3)
 
         self.root.mainloop()
 
+    def checkTypeOfEq(self, eq):
+
+        if 'x' in eq:
+            # Уравнение вида ax+b=0(пользователь не вводит 0)
+            if '=' == eq[len(eq) - 1] or '=' not in eq:
+                return 'eq0'
+            # простое уравнение вида ax+b=c
+            return 'eq'
+
+        # Простое выражение
+        if 'x' not in eq:
+            return 'exp'
+
     @staticmethod
     def setNewSymbolToEq(self, i):
+        if i not in buttons:
+            messagebox.showerror("Error!", "Enter a number or a function")
+            return
+
         if i == "C":
             self.eq = ""
             self.calc_entry.delete(0, END)
@@ -37,17 +56,32 @@ class Interface:
             self.eq += "**"
             self.calc_entry.insert(END, "**")
             return
+        if i == "=":
+            if i in self.calc_entry.get():
+                messagebox.showerror("Error!", "'=' is already in your EQ")
+                return
+        if i == ".":
+            if i == self.calc_entry.get()[len(self.calc_entry.get()) - 1]:
+                messagebox.showerror("Error!", "You can't place '.' here")
+                return
         self.eq += i
         print(self.eq)
         self.calc_entry.insert(END, i)
 
     @staticmethod
     def solveEq(self):
+        expression = self.calc_entry.get()
+        type = self.checkTypeOfEq(expression)
 
-        left = self.eq.split('=')[0]
-        right = self.eq.split('=')[1]
-        result = self.calculator.solveEq(left, right)
-        self.eq=""
+        if type == 'eq':
+            left = (self.calc_entry.get()).split('=')[0]
+            right = (self.calc_entry.get()).split('=')[1]
+            result = self.calculator.solveEq(left, right)
+        elif type == 'eq0':
+            result = self.calculator.solveEqi(str(expression).replace('=', ""))
+        elif type == 'exp':
+            result = self.calculator.solveExp(str(expression).replace('=', ""))
+        self.eq = ""
         self.calc_entry.delete(0, END)
         self.calc_entry.insert(END, "Ответ: x = " + str(result))
         print(result)
@@ -59,8 +93,7 @@ class Interface:
             cmd = lambda x=i: self.setNewSymbolToEq(self, x)
             ttk.Button(root, text=i, command=cmd, width=10).grid(row=r, column=c)
             c += 1
-            if c > 4:
+            if c > 3:
                 c = 0
                 r += 1
-        ttk.Button(root, text='SOLVE IT', command=lambda: self.solveEq(self), width=25).grid(row=1, column=c+1)
-
+        ttk.Button(root, text='SOLVE IT', command=lambda: self.solveEq(self), width=10).grid(row=0, column=3)
